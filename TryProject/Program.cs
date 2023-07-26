6a6dae744d7e710b78f43b3d5b1bd1a9f22240c3
Andrea Baravelli <Andrea.Bavarelli@Horsa.it>
2023-07-26 15:01:53 +0200

    collegamento del database al main

--- TryProject/Program.cs
+++ TryProject/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using NuGet.DependencyResolver;
 using TryProject.Data;
+using TryProject.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TryProjectContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("TryProjectContext") ?? throw new InvalidOperationException("Connection string 'TryProjectContext' not found.")));
@@ -9,6 +12,12 @@ builder.Services.AddDbContext<TryProjectContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
