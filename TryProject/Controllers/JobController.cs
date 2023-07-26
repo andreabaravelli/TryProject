f370927afa4f3c413e13c608e0ffbe9b771cca2f
Andrea Baravelli <Andrea.Bavarelli@Horsa.it>
2023-07-26 14:33:54 +0200

    change of the type of Id property into the class Job

--- TryProject/Controllers/JobController.cs
+++ TryProject/Controllers/JobController.cs
@@ -28,7 +28,7 @@ namespace TryProject.Controllers
         }
 
         // GET: Job/Details/5
-        public async Task<IActionResult> Details(string id)
+        public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.Job == null)
             {
@@ -68,7 +68,7 @@ namespace TryProject.Controllers
         }
 
         // GET: Job/Edit/5
-        public async Task<IActionResult> Edit(string id)
+        public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.Job == null)
             {
@@ -88,7 +88,7 @@ namespace TryProject.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("Id,Name")] Job job)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Job job)
         {
             if (id != job.Id)
             {
@@ -119,7 +119,7 @@ namespace TryProject.Controllers
         }
 
         // GET: Job/Delete/5
-        public async Task<IActionResult> Delete(string id)
+        public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.Job == null)
             {
@@ -139,7 +139,7 @@ namespace TryProject.Controllers
         // POST: Job/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
             if (_context.Job == null)
             {
@@ -155,7 +155,7 @@ namespace TryProject.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool JobExists(string id)
+        private bool JobExists(int id)
         {
           return (_context.Job?.Any(e => e.Id == id)).GetValueOrDefault();
         }
